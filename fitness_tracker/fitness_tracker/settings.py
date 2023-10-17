69f2dcdf882a5b88e5677d858c830f09953bc652
Kushan Choudhury <94923122+KushanChoudhury@users.noreply.github.com>
2023-10-17 22:12:45 +0530

    Update settings.py

--- fitness_tracker/fitness_tracker/settings.py
+++ fitness_tracker/fitness_tracker/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'fitness_tracker',
         'USER': 'root' ,
-        'PASSWORD': '@isPassword123',
+        'PASSWORD': '*****', #write your mysql Password here
         'HOST': 'localhost',
         'PORT' : '3306',
     }
